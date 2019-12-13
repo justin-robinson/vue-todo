0a41dd37cd1449276f4665b4b1826b4f17487077
Justin Robinson <jurobn@amazon.com>
2019-12-12 23:06:20 -0500

    can ability to complete todos

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,11 +1,12 @@
 <template>
 <div>
     <h2>This is our todos</h2>
-    <input type="text" v-model="todo">
+    <input type="text" v-model="todo.text">
     <button type="button" v-on:click="createTodo">Create Todo</button>
     <ul>
-        <li v-for="todo in todos">
-            {{ todo }}
+        <li v-for="todo in todos" v-on:click="completeTodo(todo)">
+            <span v-bind:class="{completed: todo.isCompleted}">{{ todo.text }}</span>
+            <button v-on:click="deleteTodo">delete</button>
         </li>
     </ul>
 </div>
@@ -16,19 +17,37 @@ export default {
     name: 'todos',
     data: function () {
         return {
-            todo: '',
+            todo: this.newTodo(),
             todos: []
         };
     },
     methods: {
         createTodo: function() {
             this.todos.push(this.todo);
-            this.todo = '';
+            this.todo = this.newTodo();
+        },
+        deleteTodo: function() {
+
+        },
+        completeTodo: function(todo) {
+            todo.isCompleted = !todo.isCompleted;
+        },
+        newTodo: function() {
+            return {
+                text: '',
+                isCompleted: false
+            };
         }
     }
 }
 </script>
 
 <style lang="css">
-    
+ul {
+    list-style: none;
+}
+
+.completed {
+    text-decoration: line-through;
+}
 </style>
